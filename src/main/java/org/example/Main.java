6ffd5fdfdb4051db7b9bdc376853ff0646274586
Lena Zimin <Bogypesn@gmail.com>
2024-02-27 15:36:23 -0500

    new0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello  =  world!");
+=======
+        System.out.println("Hello world!");
+        Mouse mouse = new Mouse();
+>>>>>>> 48d097f89b050b4dcfdfd1c98c2fdc08d5145ff0
     }
 }
